cf5b43d1f6e26f2b92d100057f45b81e14713715
Yash Desai <desaiyash2109@gmail.com>
2024-02-01 18:47:26 +0530

    add button created

---
+++ suggestions.js
@@ -0,0 +1,16 @@
+let suggestions = [
+    
+    "Designer",
+    "Developer",
+    "Web Designer",
+    "Web Developer",
+    "Software Engineer",
+    "Graphic Designer",
+    "Human Resource",
+    "Machine Learning Engineer",
+    "AI Engineer",
+    "Civil Engineer",
+    "Mechanical Engineer",
+    "UI/UX Designer",
+    
+];
